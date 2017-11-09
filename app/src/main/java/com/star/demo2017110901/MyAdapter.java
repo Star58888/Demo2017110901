c8a6b105481e1ea97ccb2ea8d79385353ad600cb
star <s0913940588@gmail.com>
2017-11-09 10:06:10 +0800

    11.09

--- app/src/main/java/com/star/demo2017110901/MyAdapter.java
+++ app/src/main/java/com/star/demo2017110901/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.star.demo2017110901;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,39 +17,39 @@ public class MyAdapter extends BaseAdapter {
     Context context ;
     String str [];
     public MyAdapter(Context context , String[] str)
-    {
-        this.context = context;
-        this.str = str;
+        {
+            this.context = context;
+            this.str = str;
 
-    }
-    @Override
-    public int getCount() {
-        return str.length;
-    }
+        }
+        @Override
+        public int getCount() {
+            return str.length;
+        }
 
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
 
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        //系統有的
-//        TextView tv = new TextView(context);
-//        tv.setText("Hello World" + position);
-//        return tv;
-
-        //自訂的
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View v = inflater.inflate(R.layout.myitem, null);
-        TextView tv = (TextView) v.findViewById(R.id.textView);
-        tv.setText(str[position] );
-        return tv;
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //系統有的
+    //        TextView tv = new TextView(context);
+    //        tv.setText("Hello World" + position);
+    //        return tv;
+            Log.d("GETVIEW" , "position" + position);
+            //自訂的
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View v = inflater.inflate(R.layout.myitem, null);
+            TextView tv = (TextView) v.findViewById(R.id.textView);
+            tv.setText(str[position] );
+            return v;
 
 
     }
