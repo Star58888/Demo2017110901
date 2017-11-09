2558dd9536d468259dbc771dc8762390b44010fe
star <s0913940588@gmail.com>
2017-11-09 08:50:01 +0800

    Initial commit

---
+++ app/src/main/java/com/star/demo2017110901/MainActivity.java
@@ -0,0 +1,16 @@
+package com.star.demo2017110901;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView list = findViewById(R.id.listView);
+
+    }
+}
---
+++ app/src/main/java/com/star/demo2017110901/MyAdapter.java
@@ -0,0 +1,8 @@
+package com.star.demo2017110901;
+
+/**
+ * Created by Star on 2017/11/9.
+ */
+
+public class MyAdapter {
+}
